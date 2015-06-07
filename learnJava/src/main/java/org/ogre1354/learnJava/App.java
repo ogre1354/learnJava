d0942d53e78931d954b7a6719a0e7bab8cf8e976
ogre1354 <dookie2@yahoo..com>
2015-06-07 14:07:12 -0400

    Initial project stub

---
+++ learnJava/src/main/java/org/ogre1354/learnJava/App.java
@@ -0,0 +1,13 @@
+package org.ogre1354.learnJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
